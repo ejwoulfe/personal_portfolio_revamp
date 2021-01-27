e85bc1231c586110be9db143b19aa456a6a34c5c
Ed <edwoulfe@gmail.com>
2021-01-27 15:06:24 -0500

    Completed the look of the banner. Background image, color and gradients added. The bottom section with my self image, name, and role are all in position.

--- portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import Navigation from './components/navigation/navigation';
-
-function App() {
-  return (
-    <Navigation></Navigation>
-  );
-}
-
-export default App;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Navigation from './components/navigation/navigation';
+import Banner from './components/banner/banner';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="main_content">
+      <Navigation></Navigation>
+      <Banner></Banner>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
