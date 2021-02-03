8bda33ef065be17c6d687862c7f549004a2c9d96
Ed <edwoulfe@gmail.com>
2021-02-03 12:59:16 -0500

    Right panels html and css implemented. Need to add more to the text for all projects to fill it out and add list styling to goals area.

--- portfolio/src/components/projects/panels/right_panel.jsx
+++ portfolio/src/components/projects/panels/right_panel.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './right_panel.scss';
 
 function RightPanel(props) {
 
     return (
         <div className="right_panel">
-            <h4>Right Panel</h4>
+            <div className="description">
+                <h3>Description</h3>
+                <p>{props.description}</p>
+            </div>
+
+            <div className="goals">
+                <h3>Goals</h3>
+                <p>{props.goals}</p>
+            </div>
+
+            <div className="challenges">
+                <h3>Challenges</h3>
+                <p>{props.challenges}</p>
+            </div>
         </div>
     )
 }
