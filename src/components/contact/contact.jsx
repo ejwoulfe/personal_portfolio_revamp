212b1bb139dedf1a15c06dabbf5ce457a044aed3
Ed <edwoulfe@gmail.com>
2021-02-11 10:55:23 -0500

    Added back to the top button at the bottom of the page. Added functionality to the navigation so all links will go to their corresponding sections. Some css fixes in navigation.

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './contact.scss';
-import email from '../../assets/icons/contact.svg';
 import ContactForm from './contact_form/contact_form';
+import arrowUp from '../../assets/icons/arrow-up.svg';
 
 function Contact() {
+
+    function toTopOfPage() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
     return (
+
         <div id="contact">
             <div className="header"> <h1 className="header_text">Contact</h1></div>
             <div id="contact_container">
                 <div id="contact_form">
                     <ContactForm></ContactForm>
+                </div>
+                <div id="back_to_top_container">
+                    <button id="to_top_button" onClick={() => toTopOfPage()}><img src={arrowUp} alt="button that naviagtes to the top of the page."></img></button>
+                    <p>Back to Top</p>
                 </div>
             </div>
         </div>
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import './navigation.scss';
 
 function Navigation() {
+
+    function navigateToProjects() {
+        document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+    }
+    function navigateToAbout() {
+        document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+
+    }
+    function navigateToContact() {
+        document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+
+    }
     return (
         <div id="navigation">
-
             <ul id="nav_list">
-                <li className="nav_links">Projects</li>
-                <li className="nav_links">About</li>
-                <li className="nav_links">Contact</li>
+                <li className="nav_links" onClick={() => navigateToProjects()} >Projects</li>
+                <li className="nav_links" onClick={() => navigateToAbout()}>About</li>
+                <li className="nav_links" onClick={() => navigateToContact()}>Contact</li>
             </ul>
         </div>
     )
