f0fc8a8c9ad007c43740a70e78e7adb0908ffe80
Ed Woulfe <edwoulfe@gmail.com>
2021-02-14 13:43:36 -0600

    Redesign of projects code and github svgs. Added the banner links to my github profile and resume pdf.

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './banner.scss';
 import self_image from '../../assets/mountain.jpg';
+import githubWhite from '../../assets/icons/github-white.svg';
+import resume from '../../assets/icons/resume.svg';
 
 
 function Banner() {
+
+    function goToGithub(){
+        window.open("https://github.com/ejwoulfe");
+    }
 
     return (
 
         <div id="banner">
             <div id="banner_image">
+                <div id="banner_links">
+                    <span>
+                    <img src={githubWhite} onClick={() => goToGithub()} alt="link to my github"></img>
+                    <h5>Github</h5> 
+                </span>
+                <span>
+                    <img src={resume} alt="pdf file of my resume"></img>
+                    <h5>Resume</h5>
+                </span>
+                </div>
             </div>
             <div id="personal_info">
                 <img id="self_portrait" src={self_image} alt="Me on a mountain"></img>
