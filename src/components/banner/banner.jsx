3b82e6fd8ca06ee25290a6393a21f95bb5d752bf
Ed <edwoulfe@gmail.com>
2021-03-22 11:54:53 -0400

    Removed import for build.

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './banner.scss';
 import leftShape from '../../assets/banner_images/left-shape.png';
 import rightShape from '../../assets/banner_images/right-shape.png';
