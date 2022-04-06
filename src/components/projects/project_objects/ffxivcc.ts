7f0a06fd76de329370318cd3b859ae56f497dfbf
Ed <edwoulfe@gmail.com>
2022-04-06 15:56:51 -0400

    Updated the description for FFXIV Crafting Calculator, recorded a quick demo of the website and uploaded it to youtube, added the youtube link to the ffxivcc object.

--- src/components/projects/project_objects/ffxivcc.ts
+++ src/components/projects/project_objects/ffxivcc.ts
@@ -26,7 +26,7 @@ let ffxivccProject: Project = new PersonalProject(
 },
     "none",
     "https://github.com/ejwoulfe/FFXIV-crafting-calculator",
-    "75jnHmUn5_0",
+    "jwuS_8I_jLQ",
     ["FFXIV Crafting Calculator is a full stack web app that calculates the profit margins of an in-game craft. It will give a list of all the required materials and their quantites that are needed in order to craft a recipe. Then using a third party API it retrieves the current listings of those materials on the marketboard that a user can purchase from. Using an algorithm I created it will grab the cheapest combinations of marketboard listings to meet the materials required quantity. It will gather the totals of all materials and lookup what the current marketboard value is for the recipe and calculate a profit, if it exists."]
 
 )
