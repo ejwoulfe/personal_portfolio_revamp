0bbcded653420387d70e573ffea3071d776fa7b8
Ed <edwoulfe@gmail.com>
2022-04-06 11:23:52 -0400

    Project list and project description components are now responsive, added some styling to both, also added a back button at the top of the description component if a user wants to go back to the list of projects.

--- src/components/projects/projects_container.tsx
+++ src/components/projects/projects_container.tsx
@@ -4,7 +4,7 @@ import favMoviesProject from './project_objects/favorite_movies';
 import lolChampionsProject from './project_objects/lol_champions';
 import bdoProfitProject from './project_objects/bdoprofit';
 import ffxivccProject from './project_objects/ffxivcc';
-import backArrow from '../../assets/icons/svg/back-arrow.svg';
+import backArrow from '../../assets/icons/svg/arrow-up.svg';
 import { Project } from '../../interfaces/project_interface';
 import ProjectsList from './projects_list/projects_list';
 import ProjectDescription from './project_description/project_description';
@@ -32,6 +32,7 @@ function ProjectsContainer() {
                 null :
                 <button id="projects_back_button" onClick={() => { setSelectedProject(null) }}>
                     <img src={backArrow} alt="back button" />
+                    <h3 id="go_back_text">Go Back</h3>
                 </button>
             }
 
